c9540d7dafd5661872d5f4cb3d1e2a498f4a972d
Farideh Tavakoli <tavakoli.faride@gmail.com>
2024-12-10 18:37:57 +0330

    Documentation and README file completed.

--- src/game.py
+++ src/game.py
@@ -1,12 +1,21 @@
+"""
+Aythor: Farideh Tavakoli
+Date Created: 10/12/2024
+Description: Rock Paper Scissors game.
+"""
+
 import random 
+from typing import List
 
 class RockPaperScissors:
-    def __init__(self, name):
-        self.choices = ['rock', 'paper', 'scissors']
-        self.name = name
+    """Main class for Rock Paper Scissors game.
+    """ 
+    def __init__(self, name: str):
+        self.choices: List[str] = ['rock', 'paper', 'scissors']
+        self.name: str = name
     
     def get_player_choice(self):
-        player_choice = input(f'Choose among ({self.choices}): ')
+        player_choice: str = input(f'Choose among ({self.choices}): ')
         if player_choice.lower() in self.choices:
             return player_choice.lower()
         else:
@@ -14,9 +23,16 @@ class RockPaperScissors:
             return self.get_player_choice()
 
     def get_computer_choice(self):
+        """Get computer choice randomly from choices: Rock, Paper, Scissor."""
         return random.choice(self.choices)
     
-    def decide_winner(self, player_choice, computer_choice):
+    def decide_winner(self, player_choice: str, computer_choice: str) -> str:
+        """Decide the winner based on user and computer choices. 
+
+        :param player_choice: Choice of the player
+        :param computer_choice: Choice of the computer
+        :return: The result of the game. Who is the winner?
+        """
         if player_choice == computer_choice:
             return "It's a Tie!"
         winning_combinations = [('rock', 'scissors'), ('scissors', 'paper'), ('paper', 'rock')]
@@ -28,6 +44,12 @@ class RockPaperScissors:
         return "Oh no! The Computer Won!"
 
     def play(self):
+        """Play the game.
+        - Get player choice.
+        - Get computer choice.
+        - Decide the winner.
+        - Print the result.
+        """
         player_choice = self.get_player_choice()
         computer_choice = self.get_computer_choice()
         print(f'Player choice: {player_choice} and Computer Choice: {computer_choice}')
